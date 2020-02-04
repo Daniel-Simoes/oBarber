55378f615b413e4af7b28e74188cc8406a81a1f1
daniel.simoes <daniel.simoes@hotmail.com>
2020-02-04 01:10:32 +0000

    Authentication - JWT & Middleware

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+
+export default async (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+        return res.status(401).json({ error: 'Token Not Provided' });
+
+    }
+
+    const [, token] = authHeader.split(' '); // desistrituracao do token, pois antes estava com o bearer + token
+
+
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+        req.userId = decoded.id;
+
+        return next();
+
+    } catch (err) {
+        return res.status(401).json({ error: 'Token Invalid' });
+
+    }
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret:'385907a8be7e27158ff0b1a907234c54',
+  expiresIn:'7d'
+};
