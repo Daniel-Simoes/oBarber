1cf9d561d1de9701e5d9437c909b1c87a33cc227
daniel.simoes <daniel.simoes@hotmail.com>
2020-02-17 21:20:55 +0000

    Dashboard style/Search appointments between days

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,10 +1,67 @@
-import React from 'react';
-import api from '~/services/api';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 
-// import { Container } from './styles';
 
-export default function Dashboard () {
-    api.get('appointments');
-  return <h1>oi dashboard</h1>;
+import { Container, Time} from './styles';
+
+export default function Dashboard() {
+    const [date, setDate] = useState(new Date());
+
+    const dateFormatted = useMemo(
+        () => format(date, "d 'de' MMMM", { locale: pt }),
+        [date]
+      );
+
+      function handlePrevDay() {
+        setDate(subDays(date, 1));
+      }
+
+      function handleNextDay() {
+        setDate(addDays(date, 1));
+      }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button"onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+          <Time past>
+              <strong>11:00</strong>
+              <span>
+                Daniel Simões
+              </span>
+          </Time>
+          <Time available>
+              <strong>12:00</strong>
+              <span>
+                Em Aberto
+              </span>
+          </Time>
+          <Time>
+              <strong>13:00</strong>
+              <span>
+                Daniel Simões
+              </span>
+          </Time>
+          <Time>
+              <strong>14:00</strong>
+              <span>
+                Daniel Simões
+              </span>
+          </Time>
+      </ul>
+
+    </Container>
+  );
 }
