15ca280d92bdbaf5e254e0df008b6b69ee96915c
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-09 21:36:13 +0000

    SignUp's Page

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import logo from '../../assets/logo.png';
 
- import Background from '../../components/Background';
+import Background from '../../components/Background';
 
-export default function SignUp() {
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLinkText,
+  SignLink,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>Teste</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+        <FormInput
+            icon="person-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Name"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Password"
+          />
+          <SubmitButton lonPress={() =>{}}>
+            Enter
+          </SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Sign In</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
-
   );
 }
