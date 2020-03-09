511ff3ef861e3bb7943801f3e6494a24c081f20d
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-09 03:31:23 +0000

    Creating Mobile Project

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={style.container}>
+        <Text style={style.welcome}> Welcomo To o'Barber</Text>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
