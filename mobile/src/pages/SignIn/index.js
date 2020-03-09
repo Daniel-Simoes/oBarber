2648a16a2b0495c7fc489cf44b2c38db28907191
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-09 21:25:28 +0000

    SignIn's Page

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
- import Input from '../../components/Input';
- import Button from '../../components/Button';
- import Background from '../../components/Background';
+import { Image } from 'react-native';
+import logo from '../../assets/logo.png';
+
+import Background from '../../components/Background';
+
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLinkText,
+  SignLink,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>Teste</Text>
-      <Input
-      style={{marginTop: 30}}
-      icon='call'
-      placeholder='Name'
-      />
-      <Button>Send</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+          <SubmitButton lonPress={() =>{}}>
+            Acessar
+          </SubmitButton>
+        </Form>
+        <SignLink onPress={() =>{}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
-
   );
 }
