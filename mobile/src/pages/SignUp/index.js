dd955c21d48f7e934168769d6e7c169bb159c78e
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-09 22:23:50 +0000

    Accessibility-Jumping inputs on mobile's keyboard

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image } from 'react-native';
 import logo from '../../assets/logo.png';
 
@@ -15,6 +15,11 @@ import {
 } from './styles';
 
 export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  function handleSubmit() {
+
+  }
   return (
     <Background>
       <Container>
@@ -26,6 +31,8 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Name"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
           />
           <FormInput
             icon="mail-outline"
@@ -33,17 +40,23 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Email"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Password"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
-          <SubmitButton lonPress={() =>{}}>
+          <SubmitButton lonPress={handleSubmit}>
             Enter
           </SubmitButton>
         </Form>
-        <SignLink onPress={() => navigation.navigate('SignUp')}>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
           <SignLinkText>Sign In</SignLinkText>
         </SignLink>
       </Container>
