aa43e06246495d93fe42f62dba711143af9212bf
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-09 19:25:29 +0000

    Babel Plugin Root Import

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
 
-import './config/ReactotronConfig';
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-console.tron.log('ola!')
 export default function App() {
-    return (
-      <View style={style.container}>
-        <Text style={style.welcome}> Welcome To o'Barber</Text>
-        <Text style={style.welcome}> Abaixo</Text>
-      </View>
-    );
-  }
+  return <View />;
+}
+
 
 
