e87d06964315938f4d7ce1f1e2a1e8f274535e55
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-10 01:22:26 +0000

    Routes's Style

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (signedIn = false) =>
   createAppContainer(
@@ -17,8 +18,21 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
-          }),
+            Profile
         },
+        {
+          resetOnBlur: true,
+          tabBarOptions: {
+            keyboardHidesTabBar: true,
+            activeTintColor: '#FFF',
+            inactiveTintColor: 'rgba(255,255,255, 0.7)',
+            style: {
+              backgroundColor: '#FC0061',
+            },
+          },
+        }
+        ),
+      },
         {
           initialRouteName: signedIn ? 'App' : 'Sign'
         }),
