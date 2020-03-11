8254fb65518b4ed61b5decccf25678fec8bd4efe
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-11 02:00:23 +0000

    Routes to New Appointments

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,17 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+import { createAppContainer, createSwitchNavigator,  } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirmation from './pages/New/Confirmation';
 
 export default (signedIn = false) =>
   createAppContainer(
@@ -18,7 +24,36 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
-            Profile
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirmation,
+                },{
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+                ),
+                navigationOptions: {
+                  // faz a tabBar sumir ao clickar
+                  tabBarVisible: false,
+                  tabBarLabel: 'Agendar',
+                  tabBarIcon: (
+                    <Icon
+                      name="add-circle-outline"
+                      size={20}
+                      color='#FFF'
+                    />
+                  ),
+                },
+              },
+            Profile,
         },
         {
           resetOnBlur: true,
