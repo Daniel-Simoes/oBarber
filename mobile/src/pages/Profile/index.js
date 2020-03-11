42d86926c5e8dc896eba474f8afb8480fffeb9d0
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-11 00:40:41 +0000

    Update Profile

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,19 +1,114 @@
-import React from 'react';
-
-
+import React, { useRef, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/Background';
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
-// import { Container } from './styles';
+import { FormInput, Separator, Container, LoggoutButton, Title, Form, } from './styles';
+
+import { SubmitButton } from '../SignIn/styles';
+
 
 export default function Profile() {
-  return <Background />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const oldPasswordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [password, setPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      })
+    );
   }
 
-  Profile.navigationOptions = {
-    tabBarLabel: 'Profile',
-    tabBarIcon: ({ tintColor }) => (
-      <Icon name="person" size={20} color={tintColor} />
-  ),
-};
+  return (<Background>
+            <Container>
+              <Title>My Profile</Title>
+              <Form>
+                <FormInput
+                  icon="person-outline"
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  returnKeyType="next"
+                  value={name}
+                  onChangeText={setName}
+                  onSubmitEditing={() => emailRef.current.focus()}
+                  placeholder="Name"
+                />
+                <FormInput
+                  icon="mail-outline"
+                  keyboardType="email-address"
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  ref={emailRef}
+                  value={email}
+                  onChangeText={setEmail}
+                  returnKeyType="next"
+                  onSubmitEditing={() => oldPasswordRef.current.focus()}
+                  placeholder="Email"
+                />
+                <Separator />
+                <FormInput
+                  icon="lock-outline"
+                  secureTextEntry
+                  ref={oldPasswordRef}
+                  value={oldPassword}
+                  onChangeText={setOldPassword}
+                  returnKeyType="next"
+                  onSubmitEditing={() => passwordRef.current.focus()}
+                  placeholder="Old Password"
+                />
+                <FormInput
+                  icon="lock-outline"
+                  secureTextEntry
+                  returnKeyType="next"
+                  value={password}
+                  onChangeText={setPassword}
+                  onSubmitEditing={() => confirmPasswordRef.current.focus()}
+                  ref={passwordRef}
+                  placeholder="New Password"
+                />
+                <FormInput
+                  icon="lock-outline"
+                  secureTextEntry
+                  returnKeyType="send"
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  onSubmitEditing={handleSubmit}
+                  ref={confirmPasswordRef}
+                  placeholder="Confirm the Password"
+                />
+                <SubmitButton onPress={handleSubmit}>Update Profile</SubmitButton>
+              </Form>
+            </Container>
+          </Background>
+  );
+}
+      Profile.navigationOptions = {
+        tabBarLabel: 'Meu perfil',
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="person" size={20} color={tintColor} />
+        ),
+      };
---
+++ mobile/src/pages/Profile/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: #BBB;
+  margin: 20px 0 30px;
+`;
+
+export const LoggoutButton = styled(Button)`
+  margin-top: 10px;
+  background: #BBB;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #BBB;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  contentContainerStyle: { padding: 30 },
+  showsVerticalScrollIndicator: false,
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
