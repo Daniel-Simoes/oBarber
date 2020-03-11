10298c3b212c01f1d8afe32f61af3a9291b2d487
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-11 02:59:05 +0000

    DatePicker (Schedule's Page) To IOS and Android

---
+++ mobile/src/components/DanteTimeInput/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+  margin: 60px 0 30px;
+`;
+
+export const DateButton = styled.TouchableOpacity`
+  padding: 0 15px;
+  height: 70px;
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 50px;
+  margin: 0 30px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DataText = styled.Text`
+  font-size: 15px;
+  color: #FFF;
+  margin-left: 15px;
+`;
+
+export const Picker = styled.View`
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+  padding: 15px 30px;
+  margin-top: 30px;
+`;
--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-
+import { Container, } from './styles';
 import Background from '../../../components/Background';
+import DateTimeInput from '../../../components/DanteTimeInput';
 
 export default function SelectDateTime() {
-  return <Background/>;
+  const [date, setDate] = useState(new Date());
+  return <Background>
+    <Container>
+      <DateTimeInput date={date} onChange={setDate} />
+    </Container>
+  </Background>;
 }
 
-SelectDateTime.navigationOptions = {
-  title: 'Schedule',
-};
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Schedules',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color='#FFF' />
+    </TouchableOpacity>
+  ),
+});
