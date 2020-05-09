70c7cc2dffbd6fe5a988644fed802ba547b2411f
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-09 03:47:15 +0100

    Fix header and changed default color

--- mobile/src/components/Background/index.js
+++ mobile/src/components/Background/index.js
@@ -2,7 +2,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
 export default styled(LinearGradient).attrs({
-  colors: ['#1fc9FD', '#FC0061'],
+  colors: ['#036885', '#504758'],
 })`
   flex: 1;
 `;
